9e10c466df5e13dd1d29c9d1722fac976d85e176
Caio César Gallo <caio.gallo@gmail.com>
2017-04-11 16:41:15 +0000

    testes unitarios

--- xy-inc/src/main/java/com/inc/xy/rest/controller/XYController.java
+++ xy-inc/src/main/java/com/inc/xy/rest/controller/XYController.java
@@ -1,5 +1,9 @@
 package com.inc.xy.rest.controller;
 
+import com.inc.xy.model.Document;
+import com.inc.xy.model.repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author caio
  */
 @RestController
-@RequestMapping("/xy-inc/v1")
+@RequestMapping("/xy-inc/v1/{model}")
 public class XYController {
+    
+    @Autowired
+    private DocumentRepository documentRepository;
     
     @RequestMapping("/hello")
     public void hello(){
         System.out.println("hello");
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    public void getAll(@PathVariable("model") String model){
+        Document findByModel = documentRepository.findByModel(model);
+        System.out.println(findByModel);
+    }
 }
---
+++ xy-inc/src/test/java/com/inc/xy/XYIncApplicationTest.java
@@ -0,0 +1,26 @@
+package com.inc.xy;
+
+import com.inc.xy.rest.controller.XYController;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author caio
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XYIncApplicationTest {
+    @Autowired
+    private XYController controller;
+    
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNull();
+    }
+}
